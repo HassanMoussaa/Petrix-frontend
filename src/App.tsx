0e64538ed1364d690278122222a0436641aa1c29
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-10 15:10:15 +0300

    Created sign-in page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-         Hello World
+           <BrowserRouter>
+      <Routes>
+        {/* <Route path='/' element={<Signin/>} /> */}
+        {/* <Route path='/signup' element={<Signup />} /> */}
+        {/* <Route path="/homepage" >
+          <Route index element={<Homepage/>} />
+        </Route> */}
+        
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Signin.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Signin = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const submit = async () => {
+    if (!email || !password) {
+      setError("Please provide both email and password.");
+      return;
+    }
+
+    try {
+      const result = await axios.post("http://127.0.0.1:8000/api/login", {
+        email,
+        password,
+      });
+
+      localStorage.setItem("jwt_token", result.data.authorization.token);
+      navigate("/Homepage");
+    } catch (error) {
+      setError(error.response.data.message);
+      console.error("An error occurred during login:", error);
+    }
+  };
+
+  const signUp = () => {
+    navigate("/signup");
+  };
+
+  return (
+    <div className="signin_container">
+      <img className="sidenav__logo" src={logo} alt=" Logo" />
+      <div>
+        <h2>Sign In</h2>
+        <input
+          type="text"
+          value={email}
+          placeholder="Email"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+      </div>
+      <div>
+        <input
+          type="password"
+          value={password}
+          placeholder="Password"
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+      </div>
+      {error && <p className="error_message">{error}</p>}
+      <div className="signin_signup">
+        <div>
+          <button onClick={submit}>Sign In</button>
+        </div>
+        <div>
+          <button onClick={signUp}>Sign Up</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
