ac0e9c0b987f1321377e935ef588ef4fffde1da4
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-11 14:35:12 +0300

    Updated name signup page to Login page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
 
 import './App.css';
-import Signin from './pages/Signin';
+import Login from './pages/Login';
 
 function App() {
   return (
     <div className="App">
            <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Signin/>} />
+        <Route path='/' element={<Login/>} />
         {/* <Route path='/signup' element={<Signup />} /> */}
         {/* <Route path="/homepage" >
           <Route index element={<Homepage/>} />
