73d10fb806a91045e0cecd7360b34522e3be5b68
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-11 17:08:05 +0300

    Added path for signup page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
 
 import './App.css';
 import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
            <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login/>} />
-        {/* <Route path='/signup' element={<Signup />} /> */}
+        <Route path='/signup' element={<Signup />} />
         {/* <Route path="/homepage" >
           <Route index element={<Homepage/>} />
         </Route> */}
