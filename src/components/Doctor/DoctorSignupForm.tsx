e79f6d0da7d4d9555b09621da53a1ccda753ef43
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-12 14:16:17 +0300

    Fixed doctor signup form component to handle multiple select & typescript errors

--- src/components/Doctor/DoctorSignupForm.tsx
+++ src/components/Doctor/DoctorSignupForm.tsx
@@ -15,20 +15,20 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import { countries } from '../../utils/countries';
-import { petSpecialties } from '../../utils/petSpecialties';
+import { countries } from "../../utils/countries";
+import { petSpecialties } from "../../utils/petSpecialties";
 import { SelectChangeEvent } from "@mui/material/Select";
 
 interface SignupFormProps {
   signUpError: boolean;
   handleSubmit: React.FormEventHandler<HTMLFormElement>;
-  setSignUpError:React.Dispatch<React.SetStateAction<boolean>>
-  emailUsedError:boolean;
-  setEmailUsedError:React.Dispatch<React.SetStateAction<boolean>>
-  passwordMatch:boolean;
+  setSignUpError: React.Dispatch<React.SetStateAction<boolean>>;
+  emailUsedError: boolean;
+  setEmailUsedError: React.Dispatch<React.SetStateAction<boolean>>;
+  passwordMatch: boolean;
 }
 
-function DoctorSignupForm(props:SignupFormProps) {
+function DoctorSignupForm(props: SignupFormProps) {
   const {
     signUpError,
     setSignUpError,
@@ -38,29 +38,25 @@ function DoctorSignupForm(props:SignupFormProps) {
     handleSubmit,
   } = props;
 
+  const [selectedCountry, setSelectedCountry] = useState<string>("");
+  const [selectedSpeciality, setSelectedSpeciality] = useState<string[]>([""]);
 
+  const handleChange = (event: SelectChangeEvent<string>) => {
+    setSelectedCountry(event.target.value);
+  };
 
- const [selectedCountry, setSelectedCountry] = useState<string>("");
- const [selectedSpeciality, setSelectedSpeciality] = useState<string>("");
-
-
-
-const handleChange = (event: SelectChangeEvent<string>) => {
-  setSelectedCountry(event.target.value);
-}
-
-const handleChangeSpeciality = (event: SelectChangeEvent<string>) => {
-  setSelectedSpeciality(event.target.value);
-}
+  const handleChangeSpeciality = (
+    event: SelectChangeEvent<typeof petSpecialties>
+  ) => {
+    const {
+      target: { value },
+    } = event;
+    setSelectedSpeciality(typeof value === "string" ? value.split(",") : value);
+  };
 
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-    >
-      <Box  style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}>
+    <Grid container direction="row" justifyContent="center" alignItems="center">
+      <Box style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}>
         <Typography
           id="transition-modal-title"
           sx={{ mb: 5 }}
@@ -92,8 +88,6 @@ const handleChangeSpeciality = (event: SelectChangeEvent<string>) => {
             Email already used!
           </Alert>
         )}
-
-     
 
         <Box component="form" onSubmit={handleSubmit} sx={{ mb: 5 }}>
           <TextField
@@ -159,26 +153,24 @@ const handleChangeSpeciality = (event: SelectChangeEvent<string>) => {
           />
 
           {!passwordMatch && (
-            <Typography sx={{ mb: 2 }}>
-              Passwords didn't match!
-            </Typography>
+            <Typography sx={{ mb: 2 }}>Passwords didn't match!</Typography>
           )}
 
           <InputLabel id="country-label">Country</InputLabel>
-                <Select
+          <Select
             labelId="country-label"
             id="country"
             value={selectedCountry}
             onChange={handleChange}
             required
             sx={{ mb: 3 }}
-            >
+          >
             {countries.map((country) => (
-                <MenuItem key={country.label} value={country.label}>
+              <MenuItem key={country.label} value={country.label}>
                 {country.label}
-                </MenuItem>
+              </MenuItem>
             ))}
-            </Select>
+          </Select>
 
           {/* <FormLabel component="legend">Gender</FormLabel> */}
           {/* <RadioGroup row aria-label="gender" name="gender" defaultValue="0">
@@ -203,7 +195,7 @@ const handleChangeSpeciality = (event: SelectChangeEvent<string>) => {
             />
           </RadioGroup> */}
 
-            <TextField
+          <TextField
             required
             fullWidth
             name="city"
@@ -214,22 +206,22 @@ const handleChangeSpeciality = (event: SelectChangeEvent<string>) => {
             sx={{ mb: 3 }}
           />
 
-
-           <InputLabel id="speciality">Speciality</InputLabel>
-            <Select
-              labelId="speciality"
-              id="speciality"
-              value={selectedSpeciality}
-              onChange={handleChangeSpeciality}
-              required
-              sx={{ mb: 3 }}
-            >
-              {petSpecialties.map((specialty) => (
-                <MenuItem key={specialty} value={specialty}>
-                  {specialty}
-                </MenuItem>
-              ))}
-            </Select>
+          <InputLabel id="speciality">Speciality</InputLabel>
+          <Select
+            labelId="speciality"
+            id="speciality"
+            multiple
+            value={selectedSpeciality}
+            onChange={handleChangeSpeciality}
+            required
+            sx={{ mb: 3 }}
+          >
+            {petSpecialties.map((specialty) => (
+              <MenuItem key={specialty} value={specialty}>
+                {specialty}
+              </MenuItem>
+            ))}
+          </Select>
 
           <Button
             type="submit"
