bbf8226f721db8a383be212b0fd4371526cde5ba
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-12 15:38:18 +0300

    Updated login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes,  } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import './App.css';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
+import "./App.css";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <div className="App">
-           <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Login/>} />
-        <Route path='/signup' element={<Signup />} />
-        {/* <Route path="/homepage" >
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          {/* <Route path="/homepage" >
           <Route index element={<Homepage/>} />
         </Route> */}
-        
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
