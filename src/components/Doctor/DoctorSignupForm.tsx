dbf4f62cb9d659721312674eb5caeb52ebe51fbe
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 12:00:56 +0300

    Added some styling

--- src/components/Doctor/DoctorSignupForm.tsx
+++ src/components/Doctor/DoctorSignupForm.tsx
@@ -12,6 +12,8 @@ import {
   Select,
   MenuItem,
   Alert,
+  OutlinedInput,
+  FormControl,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import { useState } from "react";
@@ -19,6 +21,7 @@ import { countries } from "../../utils/countries";
 import { petSpecialties } from "../../utils/petSpecialties";
 import { SelectChangeEvent } from "@mui/material/Select";
 
+// import "../PetOwner/petOwnerSignupForm.css";
 interface SignupFormProps {
   signUpError: boolean;
   handleSubmit: React.FormEventHandler<HTMLFormElement>;
@@ -38,7 +41,7 @@ function DoctorSignupForm(props: SignupFormProps) {
     handleSubmit,
   } = props;
 
-  const [selectedCountry, setSelectedCountry] = useState<string>("");
+  const [selectedCountry, setSelectedCountry] = useState<string>("Lebanon");
   const [selectedSpeciality, setSelectedSpeciality] = useState<string[]>([""]);
 
   const handleChange = (event: SelectChangeEvent<string>) => {
@@ -51,177 +54,181 @@ function DoctorSignupForm(props: SignupFormProps) {
     const {
       target: { value },
     } = event;
-    setSelectedSpeciality(typeof value === "string" ? value.split(",") : value);
+    if (selectedSpeciality?.length <= 4) {
+      setSelectedSpeciality(
+        typeof value === "string" ? value.split(",") : value
+      );
+    } else {
+      // TODO: add a toast message: Please select only
+    }
   };
 
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-      <Box style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}>
-        <Typography
-          id="transition-modal-title"
-          sx={{ mb: 5 }}
-          style={{ textAlign: "center" }}
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ mb: 5 }}
+      className="formBox_fields_signup"
+    >
+      {signUpError && (
+        <Alert
+          severity="error"
+          onClose={() => {
+            setSignUpError(false);
+          }}
+          sx={{ mb: 2 }}
         >
-          Create a New Account
+          Something went wrong! Try again.
+        </Alert>
+      )}
+
+      {emailUsedError && (
+        <Alert
+          severity="error"
+          onClose={() => {
+            setEmailUsedError(false);
+          }}
+          sx={{ mb: 2 }}
+        >
+          Email already used!
+        </Alert>
+      )}
+
+      <TextField
+        sx={{ mb: 3 }}
+        required
+        id="firstName"
+        name="firstName"
+        label="firstName"
+        placeholder="firstName"
+        inputProps={{
+          minLength: 2,
+        }}
+        className="input_signup_field"
+      />
+      <TextField
+        sx={{ mb: 3 }}
+        required
+        id="lastName"
+        name="lastName"
+        label="lastName"
+        placeholder="lastName"
+        inputProps={{
+          minLength: 2,
+        }}
+        className="input_signup_field"
+      />
+
+      <TextField
+        sx={{ mb: 3 }}
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        inputProps={{
+          type: "email",
+          maxLength: 100,
+        }}
+        className="input_signup_field"
+      />
+
+      <TextField
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        sx={{ mb: 3 }}
+        inputProps={{
+          minLength: 6,
+        }}
+        className="input_signup_field"
+      />
+
+      <TextField
+        required
+        fullWidth
+        name="confirm_password"
+        label="Confirm Password"
+        type="password"
+        id="confirmPassword"
+        autoComplete="current-password"
+        sx={{ mb: 3 }}
+        className="input_signup_field"
+      />
+
+      {!passwordMatch && (
+        <Typography sx={{ mb: 2 }}>Passwords didn't match!</Typography>
+      )}
+
+      <InputLabel id="country">Country</InputLabel>
+      <Select
+        name="country"
+        labelId="country"
+        id="country"
+        value={selectedCountry}
+        onChange={handleChange}
+        required
+        sx={{ mb: 3 }}
+        className="input_signup_field"
+      >
+        {countries.map((country) => (
+          <MenuItem key={country.label} value={country.label}>
+            {country.label}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <TextField
+        required
+        fullWidth
+        name="city"
+        label="city"
+        type="city"
+        id="city"
+        autoComplete="city"
+        sx={{ mb: 3 }}
+        className="input_signup_field"
+      />
+
+      <InputLabel id="speciality-select-label">Speciality</InputLabel>
+      <Select
+        className="input_signup_field"
+        labelId="speciality-select-label"
+        id="speciality"
+        multiple
+        value={selectedSpeciality}
+        onChange={handleChangeSpeciality}
+        required
+        sx={{ mb: 3 }}
+        input={<OutlinedInput label="Speciality" />}
+      >
+        {petSpecialties.map((specialty) => (
+          <MenuItem key={specialty} value={specialty}>
+            {specialty}
+          </MenuItem>
+        ))}
+      </Select>
+
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        sx={{ mt: 3, mb: 1 }}
+        className="button_signup_field"
+      >
+        Sign Up
+      </Button>
+      <Link to={"/login"} style={{ color: "#1976d2" }}>
+        <Typography style={{ color: "#212529" }}>
+          Already have an account?
         </Typography>
-
-        {signUpError && (
-          <Alert
-            severity="error"
-            onClose={() => {
-              setSignUpError(false);
-            }}
-            sx={{ mb: 2 }}
-          >
-            Something went wrong! Try again.
-          </Alert>
-        )}
-
-        {emailUsedError && (
-          <Alert
-            severity="error"
-            onClose={() => {
-              setEmailUsedError(false);
-            }}
-            sx={{ mb: 2 }}
-          >
-            Email already used!
-          </Alert>
-        )}
-
-        <Box component="form" onSubmit={handleSubmit} sx={{ mb: 5 }}>
-          <TextField
-            sx={{ mb: 3 }}
-            required
-            id="firstName"
-            name="firstName"
-            label="firstName"
-            placeholder="firstName"
-            inputProps={{
-              minLength: 2,
-            }}
-          />
-          <TextField
-            sx={{ mb: 3 }}
-            required
-            id="lastName"
-            name="lastName"
-            label="lastName"
-            placeholder="lastName"
-            inputProps={{
-              minLength: 2,
-            }}
-          />
-
-          <TextField
-            sx={{ mb: 3 }}
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            inputProps={{
-              type: "email",
-              maxLength: 100,
-            }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            sx={{ mb: 3 }}
-            inputProps={{
-              minLength: 6,
-            }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            name="confirm_password"
-            label="Confirm Password"
-            type="password"
-            id="confirmPassword"
-            autoComplete="current-password"
-            sx={{ mb: 3 }}
-          />
-
-          {!passwordMatch && (
-            <Typography sx={{ mb: 2 }}>Passwords didn't match!</Typography>
-          )}
-
-          <InputLabel id="country">Country</InputLabel>
-          <Select
-            name="country"
-            labelId="country"
-            id="country"
-            value={selectedCountry}
-            onChange={handleChange}
-            required
-            sx={{ mb: 3 }}
-          >
-            {countries.map((country) => (
-              <MenuItem key={country.label} value={country.label}>
-                {country.label}
-              </MenuItem>
-            ))}
-          </Select>
-
-          <TextField
-            required
-            fullWidth
-            name="city"
-            label="city"
-            type="city"
-            id="city"
-            autoComplete="city"
-            sx={{ mb: 3 }}
-          />
-
-          <InputLabel id="speciality">Speciality</InputLabel>
-          <Select
-            labelId="speciality"
-            id="speciality"
-            multiple
-            value={selectedSpeciality}
-            onChange={handleChangeSpeciality}
-            required
-            sx={{ mb: 3 }}
-          >
-            {petSpecialties.map((specialty) => (
-              <MenuItem key={specialty} value={specialty}>
-                {specialty}
-              </MenuItem>
-            ))}
-          </Select>
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            sx={{ mt: 3, mb: 1 }}
-          >
-            Sign Up
-          </Button>
-        </Box>
-
-        <Grid container justifyContent="space-between">
-          <Typography style={{ color: "#212529" }}>
-            Already have an account?
-          </Typography>
-
-          <Link to={"/login"} style={{ color: "#1976d2" }}>
-            Log in
-          </Link>
-        </Grid>
-      </Box>
-    </Grid>
+      </Link>
+    </Box>
   );
 }
 
--- src/components/PetOwner/PetOwnerSignupForm.tsx
+++ src/components/PetOwner/PetOwnerSignupForm.tsx
@@ -44,164 +44,129 @@ function PetSignupForm(props: SignupFormProps) {
     setSelectedCountry(event.target.value);
   };
   return (
-    <Grid container direction="row" justifyContent="center" alignItems="center">
-      <Box
-        style={{ backgroundColor: "rgba(255, 255, 255, 0.9)" }}
-        className="formBox_signup"
-      >
-        <div className="signin_logo">
-          <img
-            src={process.env.PUBLIC_URL + "/images/petrix-logo.png"}
-            alt="logo"
-          ></img>
-        </div>
-        {signUpError && (
-          <Alert
-            severity="error"
-            onClose={() => {
-              setSignUpError(false);
-            }}
-            sx={{ mb: 2 }}
-          >
-            Something went wrong! Try again.
-          </Alert>
-        )}
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{ mb: 5 }}
+      className="formBox_fields_signup"
+    >
+      <TextField
+        sx={{ mb: 3 }}
+        required
+        id="firstName"
+        name="firstName"
+        label="firstName"
+        placeholder="firstName"
+        className="input_signup_field"
+        inputProps={{
+          minLength: 2,
+        }}
+      />
+      <TextField
+        sx={{ mb: 3 }}
+        required
+        id="lastName"
+        name="lastName"
+        label="lastName"
+        placeholder="lastName"
+        className="input_signup_field"
+        inputProps={{
+          minLength: 2,
+        }}
+      />
 
-        {emailUsedError && (
-          <Alert
-            severity="error"
-            onClose={() => {
-              setEmailUsedError(false);
-            }}
-            sx={{ mb: 2 }}
-          >
-            Email already used!
-          </Alert>
-        )}
+      <TextField
+        sx={{ mb: 3 }}
+        required
+        fullWidth
+        id="email"
+        label="Email Address"
+        name="email"
+        autoComplete="email"
+        className="input_signup_field"
+        inputProps={{
+          type: "email",
+          maxLength: 100,
+        }}
+      />
 
-        <Box
-          component="form"
-          onSubmit={handleSubmit}
-          sx={{ mb: 5 }}
-          className="formBox_fields_signup"
+      <TextField
+        required
+        fullWidth
+        name="password"
+        label="Password"
+        type="password"
+        id="password"
+        autoComplete="current-password"
+        className="input_signup_field"
+        sx={{ mb: 3 }}
+        inputProps={{
+          minLength: 6,
+        }}
+      />
+
+      <TextField
+        required
+        fullWidth
+        name="confirm_password"
+        label="Confirm Password"
+        type="password"
+        id="confirmPassword"
+        className="input_signup_field"
+        autoComplete="current-password"
+        sx={{ mb: 3 }}
+      />
+
+      {!passwordMatch && (
+        <Typography sx={{ mb: 2 }}>Passwords didn't match!</Typography>
+      )}
+      <div className="country_city_group">
+        <Select
+          name="country"
+          labelId="country"
+          id="country"
+          className="input_signup_field_updated"
+          value={selectedCountry}
+          onChange={handleChange}
+          required
+          sx={{ mb: 3 }}
         >
-          <TextField
-            sx={{ mb: 3 }}
-            required
-            id="firstName"
-            name="firstName"
-            label="firstName"
-            placeholder="firstName"
-            className="input_signup_field"
-            inputProps={{
-              minLength: 2,
-            }}
-          />
-          <TextField
-            sx={{ mb: 3 }}
-            required
-            id="lastName"
-            name="lastName"
-            label="lastName"
-            placeholder="lastName"
-            className="input_signup_field"
-            inputProps={{
-              minLength: 2,
-            }}
-          />
+          {countries.map((country) => (
+            <MenuItem key={country.label} value={country.label}>
+              {country.label}
+            </MenuItem>
+          ))}
+        </Select>
 
-          <TextField
-            sx={{ mb: 3 }}
-            required
-            fullWidth
-            id="email"
-            label="Email Address"
-            name="email"
-            autoComplete="email"
-            className="input_signup_field"
-            inputProps={{
-              type: "email",
-              maxLength: 100,
-            }}
-          />
+        <TextField
+          required
+          fullWidth
+          name="city"
+          label="city"
+          type="city"
+          id="city"
+          className="input_signup_field_updated"
+          autoComplete="city"
+          sx={{ mb: 3 }}
+        />
+      </div>
 
-          <TextField
-            required
-            fullWidth
-            name="password"
-            label="Password"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            className="input_signup_field"
-            sx={{ mb: 3 }}
-            inputProps={{
-              minLength: 6,
-            }}
-          />
-
-          <TextField
-            required
-            fullWidth
-            name="confirm_password"
-            label="Confirm Password"
-            type="password"
-            id="confirmPassword"
-            className="input_signup_field"
-            autoComplete="current-password"
-            sx={{ mb: 3 }}
-          />
-
-          {!passwordMatch && (
-            <Typography sx={{ mb: 2 }}>Passwords didn't match!</Typography>
-          )}
-          <Select
-            name="country"
-            labelId="country"
-            id="country"
-            className="input_signup_field"
-            value={selectedCountry}
-            onChange={handleChange}
-            required
-            sx={{ mb: 3 }}
-          >
-            {countries.map((country) => (
-              <MenuItem key={country.label} value={country.label}>
-                {country.label}
-              </MenuItem>
-            ))}
-          </Select>
-
-          <TextField
-            required
-            fullWidth
-            name="city"
-            label="city"
-            type="city"
-            id="city"
-            className="input_signup_field"
-            autoComplete="city"
-            sx={{ mb: 3 }}
-          />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            className="button_signup_field"
-            sx={{ mt: 3, mb: 1 }}
-          >
-            Sign Up
-          </Button>
-          <Typography
-            style={{ color: "#212529" }}
-            className="already_have_account_btn"
-          >
-            <Link to={"/login"}> Already have an account?</Link>
-          </Typography>
-        </Box>
-      </Box>
-    </Grid>
+      <Button
+        type="submit"
+        fullWidth
+        variant="contained"
+        className="button_signup_field"
+        sx={{ mt: 3, mb: 1 }}
+      >
+        Sign Up
+      </Button>
+      <Typography
+        style={{ color: "#212529" }}
+        className="already_have_account_btn"
+      >
+        <Link to={"/login"}> Already have an account?</Link>
+      </Typography>
+    </Box>
   );
 }
 
