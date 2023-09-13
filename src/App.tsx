43c5e3c20f1ebcbc99a741af5ae110959e07661f
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 13:48:54 +0300

    Added route to hero page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import HeroPage from "./pages/Home/HeroPage";
 
 function App() {
   return (
@@ -12,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<HeroPage />} />
+
           {/* <Route path="/homepage" >
           <Route index element={<Homepage/>} />
         </Route> */}
