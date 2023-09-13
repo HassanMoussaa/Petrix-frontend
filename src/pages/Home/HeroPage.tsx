1d4357e6268352cb0ec3b1ccc91d9b1ade3c326e
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 15:06:15 +0300

    Added welcome component to heropage

--- src/pages/Home/HeroPage.tsx
+++ src/pages/Home/HeroPage.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainNavBar from "../../components/NavBar/MainNavBar";
+import Welcome from "../../components/User/WelcomeSection/Welcome";
 
 function HeroPage() {
   return (
     <div>
       <MainNavBar />
+      <Welcome />
     </div>
   );
 }
