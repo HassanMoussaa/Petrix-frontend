e57f4319cbe639ed425620f0b33702bec53cf81d
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 16:22:48 +0300

    Adjusted spacing between elements

--- src/components/User/WelcomeSection/Welcome.tsx
+++ src/components/User/WelcomeSection/Welcome.tsx
@@ -4,19 +4,30 @@ import { Grid, Box, Button, Typography, TextField, Alert } from "@mui/material";
 function welcome() {
   return (
     <Box sx={{ flexGrow: 5 }}>
-      <Grid container sx={{ mt: 15 }}>
-        <Grid xs={6}>
-          <Grid sx={{ fontSize: 42 }}>
-            Until one has loved an <br /> animal, a part of one's soul <br />
-            remains unawakened
+      <Grid container sx={{ mt: 15, justifyContent: "center" }}>
+        <Grid
+          xs={12}
+          md={4}
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: 5,
+            mt: 5,
+          }}
+        >
+          <Grid sx={{ fontSize: 35 }}>
+            <b>
+              Until one has loved an <br /> animal, a part of one's soul <br />
+              remains unawakened
+            </b>
           </Grid>
-          <Grid sx={{ fontSize: 23 }}>
+          <Grid sx={{ fontSize: 16 }}>
             Discover the magic of pet companionship! Click
             <br /> here to explore heartwarming stories and tips for
             <br /> caring for your furry friends.
           </Grid>
         </Grid>
-        <Grid xs={6}>
+        <Grid md={3} sx={{ display: { xs: "none", md: "flex" } }}>
           <div className="photo">
             <img
               src={process.env.PUBLIC_URL + "/images/Heropage_photo.png"}
