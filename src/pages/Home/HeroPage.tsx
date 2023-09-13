58ba0647ea86d2ace928872d166207ad47a7d718
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 16:23:30 +0300

    Imported css

--- src/pages/Home/HeroPage.tsx
+++ src/pages/Home/HeroPage.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import MainNavBar from "../../components/NavBar/MainNavBar";
 import Welcome from "../../components/User/WelcomeSection/Welcome";
+import "./heropage.css";
 
 function HeroPage() {
   return (
-    <div>
+    <div className="heropage_body">
       <MainNavBar />
       <Welcome />
     </div>
