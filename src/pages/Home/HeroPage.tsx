7e2b20a38aaad54b7a1e5b320686e17bcee2a954
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 18:56:38 +0300

    Adding first card component in heropage

--- src/pages/Home/HeroPage.tsx
+++ src/pages/Home/HeroPage.tsx
@@ -1,13 +1,24 @@
 import React from "react";
 import MainNavBar from "../../components/NavBar/MainNavBar";
 import Welcome from "../../components/User/WelcomeSection/Welcome";
+import Card from "../../components/User/WelcomeSection/Card";
+
 import "./heropage.css";
+import { Grid, Box, Button, Typography, TextField, Alert } from "@mui/material";
 
 function HeroPage() {
   return (
     <div className="heropage_body">
       <MainNavBar />
       <Welcome />
+
+      <Grid>
+        <Card
+          imageUrl={"Customized_pages .svg"}
+          title={"AI Image Classification"}
+          body={"Upload photo of animal to check it's type"}
+        />
+      </Grid>
     </div>
   );
 }
