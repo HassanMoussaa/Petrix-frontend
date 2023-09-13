d0c465ae7f235382ba7f6217e0f63b407f292728
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-13 13:49:37 +0300

    Added homepage file

---
+++ src/pages/Home/HeroPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainNavBar from "../../components/NavBar/MainNavBar";
+
+function HeroPage() {
+  return (
+    <div>
+      <MainNavBar />
+    </div>
+  );
+}
+
+export default HeroPage;
