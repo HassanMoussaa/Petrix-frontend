ba46f9cf44bac30f14c3f9dc76292dc66b0bbcef
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-14 21:01:14 +0300

    Added new navbar component for profile pages

---
+++ src/components/NavBar/NavBar.tsx
@@ -0,0 +1,176 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import AdbIcon from "@mui/icons-material/Adb";
+import { Link } from "react-router-dom";
+import { Grid } from "@mui/material";
+
+interface ResponsiveAppBarProps {
+  imageUrl: string | null;
+  DoctorFirstName: string;
+  DoctorLastName: string;
+}
+
+const pages = ["Profile"];
+const settings = ["Profile", "Dashboard", "Logout"];
+
+function ResponsiveAppBar(props: ResponsiveAppBarProps) {
+  const { imageUrl, DoctorFirstName, DoctorLastName } = props;
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
+    null
+  );
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
+    null
+  );
+
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" elevation={0} style={{ background: "#F3F5F8" }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            component="img"
+            src={process.env.PUBLIC_URL + "/images/Petrix-nav-logo.svg"}
+            alt="logo"
+            sx={{
+              display: { xs: "none", md: "flex" },
+            }}
+          />
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "flex", md: "flex" },
+              justifyContent: "center",
+            }}
+          >
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              // color="primary"
+              sx={{ display: { xs: "block", md: "none" } }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+                justifyContent: "center",
+              }}
+              style={{ justifyContent: "center" }}
+            >
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Box
+            component="img"
+            src={process.env.PUBLIC_URL + "/images/Petrix-nav-logo.png"}
+            alt="logo"
+            sx={{ display: { xs: "flex", md: "none" }, mr: 40 }}
+          />
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: "black", display: "block" }}
+                // TODO: make current page bold
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <div>
+            <Tooltip title="Meyawwwoo">
+              <Grid>
+                <Typography>{DoctorFirstName + DoctorLastName}</Typography>
+                <Box
+                  component="img"
+                  src={process.env.PUBLIC_URL + `/images/${imageUrl}`}
+                  alt="logo"
+                  sx={{
+                    display: {
+                      xs: "flex",
+                    },
+                    margin: "0 auto",
+                    width: "40%",
+
+                    borderRadius: 100,
+                  }}
+                />
+              </Grid>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </div>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default ResponsiveAppBar;
