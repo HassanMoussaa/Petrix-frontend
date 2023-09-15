53b5f8d1a25545fbdbc5a033a67205fed356e5dd
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-15 18:19:23 +0300

    Added doctor info section 2

---
+++ src/components/Doctor/DoctorInfoSection2.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import {
+  Grid,
+  Rating,
+  Box,
+  Button,
+  Typography,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  InputLabel,
+  Select,
+  MenuItem,
+  Alert,
+  OutlinedInput,
+  FormControl,
+} from "@mui/material";
+
+function DoctorInfoSection2() {
+  return <div></div>;
+}
+
+export default DoctorInfoSection2;
