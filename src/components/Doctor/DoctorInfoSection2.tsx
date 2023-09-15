201ebcf24c488a5f516a7707cfbf5d675f51f642
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-15 18:38:05 +0300

    Added headers for info section 2

--- src/components/Doctor/DoctorInfoSection2.tsx
+++ src/components/Doctor/DoctorInfoSection2.tsx
@@ -18,8 +18,68 @@ import {
   FormControl,
 } from "@mui/material";
 
-function DoctorInfoSection2() {
-  return <div></div>;
+interface DoctorInfoSection2 {
+  firstName: string;
+  lastName: string;
+  imageUrl: string;
+}
+
+function DoctorInfoSection2(props: DoctorInfoSection2) {
+  const { imageUrl, firstName, lastName } = props;
+
+  return (
+    <Grid
+      container
+      sx={{
+        display: { xs: "flex" },
+        ml: 10,
+        mt: 5,
+        flexDirection: { xs: "column" },
+      }}
+    >
+      <Grid
+        container
+        sx={{
+          display: { xs: "flex" },
+          flexDirection: { xs: "column" },
+        }}
+      >
+        <Grid container>
+          <Typography
+            sx={{
+              // fontWeight: "bold",
+              color: "#16A4C3",
+              fontSize: 20,
+            }}
+          >
+            Speciality
+          </Typography>
+        </Grid>
+        <Grid container>
+          <Typography
+            sx={{
+              // fontWeight: "bold",
+              color: "#16A4C3",
+              fontSize: 20,
+            }}
+          >
+            Contact Info
+          </Typography>
+        </Grid>
+        <Grid container>
+          <Typography
+            sx={{
+              // fontWeight: "bold",
+              color: "#16A4C3",
+              fontSize: 20,
+            }}
+          >
+            Profile Bio
+          </Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 }
 
 export default DoctorInfoSection2;
