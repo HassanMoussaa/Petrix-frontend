bd7953b47b82e2c96101cd74d7e1141b66ed8ce6
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-15 15:18:49 +0300

    Updated code structure in heropage

--- src/pages/Home/HeroPage.tsx
+++ src/pages/Home/HeroPage.tsx
@@ -8,8 +8,71 @@ import { Grid, Box, Typography, Alert } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import TopDoctors from "../../components/User/WelcomeSection/TopDoctors";
 
+const image_classification_card = {
+  imageUrl: "ImageClassification.svg",
+  title: "AI Image Classification",
+  body: "Upload photo of an animal to check its type",
+  navigate: "page1",
+};
+const chatbot_card = {
+  imageUrl: "ChatBot.svg",
+  title: "AI Chat Bot",
+  body: "Chat of AI",
+  navigate: "page2",
+};
+const doctors_near_you_card = {
+  imageUrl: "ImageClassification.svg",
+  title: "AI Image Classification",
+  body: "Upload photo of an animal to check its type",
+  navigate: "page1",
+};
+const customized_pages_card = {
+  imageUrl: "Customized_pages .svg",
+  title: "Customized Pages",
+  body: "Customize your profile",
+  navigate: "page3",
+};
+const handle_bookings_card = {
+  imageUrl: "HandleBookings.svg",
+  title: "Handle Bookings",
+  body: "Manage your appointments",
+  navigate: "page4",
+};
+
+interface HeroCard {
+  imageUrl: string;
+  title: string;
+  body: string;
+  navigate: string;
+}
+
+interface HeroCardMapping {
+  user: HeroCard[];
+  petOwner: HeroCard[];
+  doctor: HeroCard[];
+}
+
+const hero_cards: HeroCardMapping = {
+  user: [
+    image_classification_card,
+    chatbot_card,
+    customized_pages_card,
+    handle_bookings_card,
+  ],
+  petOwner: [image_classification_card, chatbot_card, doctors_near_you_card],
+  doctor: [customized_pages_card, handle_bookings_card],
+};
+
 function HeroPage() {
   const navigate = useNavigate();
+  // @ts-ignore
+  const loginData = JSON.parse(localStorage.getItem("login"));
+  const user_type =
+    loginData?.user_type == 1
+      ? "petOwner"
+      : loginData?.user_type == 2
+      ? "doctor"
+      : "user";
 
   return (
     <div className="heropage_body">
@@ -66,30 +129,14 @@ function HeroPage() {
           gap: 5,
         }}
       >
-        <Card
-          imageUrl={"ImageClassification.svg"}
-          title={"AI Image Classification"}
-          body={"Upload photo of an animal to check its type"}
-          onClick={() => navigate("/page1")}
-        />
-        <Card
-          imageUrl={"ChatBot.svg"}
-          title={"AI Chat Bot"}
-          body={"Chat of AI"}
-          onClick={() => navigate("/page2")}
-        />
-        <Card
-          imageUrl={"Customized_pages .svg"}
-          title={"Customized Pages"}
-          body={"Customize your profile"}
-          onClick={() => navigate("/page3")}
-        />
-        <Card
-          imageUrl={"HandleBookings.svg"}
-          title={"Handle Bookings"}
-          body={"Manage your appointments"}
-          onClick={() => navigate("/page4")}
-        />
+        {hero_cards[user_type].map((card) => (
+          <Card
+            imageUrl={card.imageUrl}
+            title={card.title}
+            body={card.body}
+            onClick={() => navigate(`/${card.navigate}`)}
+          />
+        ))}
       </Grid>
 
       <TopDoctors />
