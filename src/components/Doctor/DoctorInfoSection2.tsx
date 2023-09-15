c4d30e64433cfdf1f82339c3d74e48a072ba72ef
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-15 22:14:43 +0300

    Added info for section 2

--- src/components/Doctor/DoctorInfoSection2.tsx
+++ src/components/Doctor/DoctorInfoSection2.tsx
@@ -3,6 +3,7 @@ import {
   Grid,
   Rating,
   Box,
+  Paper,
   Button,
   Typography,
   TextField,
@@ -17,7 +18,9 @@ import {
   OutlinedInput,
   FormControl,
 } from "@mui/material";
-
+import { styled } from "@mui/material/styles";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
 interface DoctorInfoSection2 {
   firstName: string;
   lastName: string;
@@ -27,9 +30,18 @@ interface DoctorInfoSection2 {
 function DoctorInfoSection2(props: DoctorInfoSection2) {
   const { imageUrl, firstName, lastName } = props;
 
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    borderRadius: 10,
+  }));
   return (
     <Grid
       container
+      md={2.5}
       sx={{
         display: { xs: "flex" },
         ml: 10,
@@ -42,9 +54,16 @@ function DoctorInfoSection2(props: DoctorInfoSection2) {
         sx={{
           display: { xs: "flex" },
           flexDirection: { xs: "column" },
+          gap: 2,
         }}
       >
-        <Grid container>
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex" },
+            flexDirection: { xs: "column" },
+          }}
+        >
           <Typography
             sx={{
               // fontWeight: "bold",
@@ -54,28 +73,106 @@ function DoctorInfoSection2(props: DoctorInfoSection2) {
           >
             Speciality
           </Typography>
+
+          <Box sx={{ flexGrow: 1, mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+                <Item>xs=8</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>xs=4</Item>
+              </Grid>
+              <Grid item xs={4}>
+                <Item>xs=4</Item>
+              </Grid>
+              <Grid item xs={8}>
+                <Item>xs=8</Item>
+              </Grid>
+            </Grid>
+          </Box>
         </Grid>
-        <Grid container>
-          <Typography
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex" },
+            flexDirection: { xs: "column" },
+            gap: 3,
+          }}
+        >
+          <Grid
+            container
             sx={{
-              // fontWeight: "bold",
-              color: "#16A4C3",
-              fontSize: 20,
+              display: { xs: "flex" },
+              flexDirection: { xs: "column" },
+              gap: 3,
             }}
           >
-            Contact Info
-          </Typography>
-        </Grid>
-        <Grid container>
-          <Typography
+            <Grid
+              container
+              sx={{
+                display: { xs: "flex" },
+                flexDirection: { xs: "column" },
+                gap: 1,
+              }}
+            >
+              <Typography
+                sx={{
+                  // fontWeight: "bold",
+                  color: "#16A4C3",
+                  fontSize: 20,
+                }}
+              >
+                Contact Info
+              </Typography>
+            </Grid>
+            {/* Group for nb and email */}
+            <Grid
+              container
+              sx={{
+                display: { xs: "flex" },
+                flexDirection: { xs: "column" },
+                gap: 1,
+              }}
+            >
+              <Typography
+                sx={{
+                  // fontWeight: "bold",
+                  //   color: "#16A4C3",
+                  fontSize: 20,
+                }}
+              >
+                <LocalPhoneOutlinedIcon />
+              </Typography>
+              <Typography
+                sx={{
+                  // fontWeight: "bold",
+                  //   color: "#16A4C3",
+                  fontSize: 20,
+                }}
+              >
+                <EmailOutlinedIcon />
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
             sx={{
-              // fontWeight: "bold",
-              color: "#16A4C3",
-              fontSize: 20,
+              display: { xs: "flex" },
+              flexDirection: { xs: "column" },
+              gap: 2,
             }}
           >
-            Profile Bio
-          </Typography>
+            <Typography
+              sx={{
+                // fontWeight: "bold",
+                color: "#16A4C3",
+                fontSize: 20,
+              }}
+            >
+              Profile Bio
+            </Typography>
+            <Typography>NIce bio</Typography>
+          </Grid>
         </Grid>
       </Grid>
     </Grid>
