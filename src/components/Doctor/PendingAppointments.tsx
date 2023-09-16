b251edc19dca892cab93d6b491e8a6c245e4eac2
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-16 14:22:10 +0300

    Added pendingAppointments component

---
+++ src/components/Doctor/PendingAppointments.tsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+function PendingAppointments() {
+  return (
+    <Grid container sx={{ display: { xs: "flex" }, mt: 10 }}>
+      Hiii
+    </Grid>
+  );
+}
+
+export default PendingAppointments;
