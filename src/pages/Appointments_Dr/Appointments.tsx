edc94111e9fa02394019b7aa22d2c4620bfc50ec
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-16 13:23:00 +0300

    Added navBar to appointments page

--- src/pages/Appointments_Dr/Appointments.tsx
+++ src/pages/Appointments_Dr/Appointments.tsx
@@ -1,7 +1,97 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import axios from "axios";
+import getAPIBaseURL from "../../APIBaseURL";
+import DoctorInfoSection from "../../components/Doctor/DoctorInfoSection";
+import DoctorInfoSection2 from "../../components/Doctor/DoctorInfoSection2";
+import { Grid } from "@mui/material";
+import DoctorToggleSection from "../../components/Doctor/DoctorToggleSection";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import "./appointments.css";
+
+interface Specialty {
+  id: number;
+  speciality: string;
+  User_Specialties: {
+    createdAt: string;
+    updatedAt: string;
+    SpecialtyId: number;
+    UserId: number;
+  };
+}
+interface UserType {
+  id: number;
+  type: string;
+}
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  createdAt: string;
+}
+
+interface DoctorInfo {
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  country: string;
+  profile: string;
+  phone: string;
+  photoUrl: string | null;
+  email: string;
+  specialities: Specialty[];
+  userType: UserType;
+  post: Post[];
+}
 
 function Appointments() {
-  return <div>HI</div>;
+  const [doctorInfo, setDoctorInfo] = useState<DoctorInfo | null>(null);
+  let config = {};
+  let login_status = JSON.parse(localStorage.getItem("login") || "");
+
+  const token = login_status.token;
+  config = { headers: { Authorization: `Bearer ${token}` } };
+
+  // For toggle section
+  const [alignment, setAlignment] = React.useState("Blog");
+
+  const handleChange = (
+    event: React.MouseEvent<HTMLElement>,
+    newAlignment: string
+  ) => {
+    setAlignment(newAlignment);
+  };
+
+  async function fetchDoctorProfile() {
+    try {
+      const response = await axios.get(
+        getAPIBaseURL() + "/doctors/myProfile",
+        config
+      );
+
+      setDoctorInfo(response.data);
+    } catch (error) {
+      console.error("Error fetching doctor profile:", error);
+    }
+  }
+  useEffect(() => {
+    fetchDoctorProfile();
+  }, []);
+
+  return (
+    <div className="drBody">
+      {doctorInfo && (
+        <NavBar
+          imageUrl={doctorInfo.photoUrl || ""}
+          firstName={doctorInfo.firstName}
+          lastName={doctorInfo.lastName}
+          pageTitle={"Appointments"}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Appointments;
