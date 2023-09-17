2b6c0e355087f392088fc3a7203ca26a5bdb0a63
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-17 17:32:10 +0300

    Added navbar

--- src/pages/Ai_petOwner_ImgClass/Ai_imageClassification_Main.tsx
+++ src/pages/Ai_petOwner_ImgClass/Ai_imageClassification_Main.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import axios from "axios";
+import getAPIBaseURL from "../../APIBaseURL";
+import { Box, Paper, Typography, TextField, Button, Grid } from "@mui/material";
+import { useLocation } from "react-router-dom";
+
+interface UserType {
+  id: number;
+  type: string;
+}
+
+interface DoctorInfo {
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  country: string;
+  profile: string;
+  phone: string;
+  photoUrl: string | null;
+  email: string;
+  userType: UserType;
+}
 
 function Ai_imageClassification_Main() {
-  return <div></div>;
+  const [petOwnerInfo, setPetOwnerInfo] = useState<DoctorInfo | null>(null);
+  const token = JSON.parse(localStorage.getItem("login") || "").token;
+  const location = useLocation();
+  const postId = location.state?.postId;
+
+  useEffect(() => {
+    async function fetchDoctorProfile() {
+      try {
+        const response = await axios.get(
+          getAPIBaseURL() + "/petOwners/myProfile",
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        setPetOwnerInfo(response.data);
+      } catch (error) {
+        console.error("Error fetching doctor profile:", error);
+      }
+    }
+    fetchDoctorProfile();
+  }, [postId]);
+
+  return (
+    <div>
+      {petOwnerInfo && (
+        <NavBar
+          imageUrl={petOwnerInfo.photoUrl || ""}
+          firstName={petOwnerInfo.firstName}
+          lastName={petOwnerInfo.lastName}
+          pageTitle={"AI CLASSIFICATION"}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Ai_imageClassification_Main;
