55cf78dbd5e8bb56c3e9e343f9558962096656c8
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-17 17:33:01 +0300

    Added route to ai image classification page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import HeroPage from "./pages/Home/HeroPage";
 import DoctorProfile from "./pages/DoctorProfile_Dr/DoctorProfile";
 import Appointments from "./pages/Appointments_Dr/Appointments";
 import DoctorPost from "./pages/DoctorProfile_Dr/DoctorPost";
+import Ai_imageClassification_Main from "./pages/Ai_petOwner_ImgClass/Ai_imageClassification_Main";
 
 function App() {
   return (
@@ -22,6 +23,12 @@ function App() {
           <Route path="/profile" element={<DoctorProfile />} />
           <Route path="/appointments" element={<Appointments />} />
           <Route path="/post" element={<DoctorPost />} />
+
+          {/* petOwner routes */}
+          <Route
+            path="/ai_imageClassification"
+            element={<Ai_imageClassification_Main />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
