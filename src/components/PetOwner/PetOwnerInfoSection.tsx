9698378d475677194b317c94a3f38c8ad3318dac
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-19 18:42:55 +0300

    Added two new section for petOwners

---
+++ src/components/PetOwner/PetOwnerInfoSection.tsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import getAPIBaseURL from "../../APIBaseURL";
+
+import {
+  Grid,
+  Rating,
+  Box,
+  Button,
+  Typography,
+  Fab,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  InputLabel,
+  Select,
+  MenuItem,
+  Alert,
+  OutlinedInput,
+  FormControl,
+  Badge,
+  Avatar,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import EditIcon from "@mui/icons-material/Edit";
+import ChangeProfilePhoto from "../Doctor/ChangeProfilePhoto";
+
+interface PetOwnerInfoSectionProps {
+  firstName: string;
+  lastName: string;
+  imageUrl: string | undefined;
+  averageRate: number;
+  setNewImageUrl: React.Dispatch<React.SetStateAction<string | undefined>>;
+}
+
+function PetOwnerInfoSection(props: PetOwnerInfoSectionProps) {
+  const { imageUrl, firstName, lastName, averageRate, setNewImageUrl } = props;
+  // const value = 4.5;
+
+  let config = {};
+  let login_status = JSON.parse(localStorage.getItem("login") || "");
+  const token = login_status.token;
+  config = { headers: { Authorization: `Bearer ${token}` } };
+
+  return (
+    <Grid container sx={{ display: { xs: "flex" }, ml: 10, mt: 10, gap: 5 }}>
+      <Grid md={3} sx={{ display: { md: "flex" } }}>
+        <div>
+          <Badge
+            badgeContent={
+              <ChangeProfilePhoto setNewImageUrl={setNewImageUrl} />
+            }
+            color="primary"
+            anchorOrigin={{
+              vertical: "bottom",
+              horizontal: "right",
+            }}
+          >
+            <Avatar
+              alt="user_profile_picture"
+              src={imageUrl}
+              sx={{
+                height: "300px !important",
+                width: "300px !important",
+              }}
+              variant="square"
+            />
+          </Badge>
+        </div>
+      </Grid>
+      <Grid
+        container
+        md={4}
+        sx={{
+          display: { xs: "flex" },
+          flexDirection: { xs: "column" },
+          gap: 5,
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex" },
+            flexDirection: { xs: "column" },
+            gap: 2,
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: "bold",
+              fontSize: 28,
+            }}
+          >
+            Dr {firstName} {lastName}
+          </Typography>
+
+          <Rating
+            name="read-only"
+            value={averageRate}
+            precision={0.5}
+            readOnly
+          />
+        </Grid>
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex" },
+            gap: 2,
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              bgcolor: "#16A4C3",
+              borderRadius: 3,
+              maxWidth: 100,
+              height: 50,
+              "&:hover": {
+                backgroundColor: "#000",
+              },
+            }}
+            size="large"
+          >
+            Edit
+          </Button>
+          <Button
+            variant="contained"
+            href="/appointments"
+            sx={{
+              bgcolor: "#000",
+              borderRadius: 3,
+              maxWidth: 150,
+              height: 50,
+              "&:hover": {
+                backgroundColor: "#16A4C3",
+              },
+            }}
+            size="large"
+          >
+            Appointments
+          </Button>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default PetOwnerInfoSection;
---
+++ src/components/PetOwner/PetOwnerInfoSection2.tsx
@@ -0,0 +1,202 @@
+import React from "react";
+import {
+  Grid,
+  Rating,
+  Box,
+  Paper,
+  Button,
+  Typography,
+  TextField,
+  Radio,
+  RadioGroup,
+  FormLabel,
+  FormControlLabel,
+  InputLabel,
+  Select,
+  MenuItem,
+  Alert,
+  OutlinedInput,
+  FormControl,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import LocalPhoneOutlinedIcon from "@mui/icons-material/LocalPhoneOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+
+interface Specialty {
+  id: number;
+  speciality: string;
+  User_Specialties: {
+    createdAt: string;
+    updatedAt: string;
+    SpecialtyId: number;
+    UserId: number;
+  };
+}
+interface PetOwnerInfoSection2Props {
+  phoneNum: string;
+  profileBio: string;
+  drEmail: string;
+  specialityList: Specialty[];
+}
+
+function PetOwnerInfoSection2(props: PetOwnerInfoSection2Props) {
+  const { phoneNum, profileBio, drEmail, specialityList } = props;
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+    borderRadius: 10,
+  }));
+  return (
+    <Grid
+      container
+      md={2.5}
+      sx={{
+        display: { xs: "flex" },
+        ml: 10,
+        mt: 5,
+        flexDirection: { xs: "column" },
+      }}
+    >
+      <Grid
+        container
+        sx={{
+          display: { xs: "flex" },
+          flexDirection: { xs: "column" },
+          gap: 2,
+        }}
+      >
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex" },
+            flexDirection: { xs: "column" },
+          }}
+        >
+          <Typography
+            sx={{
+              // fontWeight: "bold",
+              color: "#16A4C3",
+              fontSize: 20,
+            }}
+          >
+            Speciality
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, mt: 1 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={8}>
+                {specialityList[0] && (
+                  <Item>{specialityList[0].speciality}</Item>
+                )}
+              </Grid>
+              <Grid item xs={4}>
+                {specialityList[1] && (
+                  <Item>{specialityList[1].speciality}</Item>
+                )}
+              </Grid>
+              <Grid item xs={4}>
+                {specialityList[2] && (
+                  <Item>{specialityList[2].speciality}</Item>
+                )}
+              </Grid>
+              <Grid item xs={8}>
+                {specialityList[3] && (
+                  <Item>{specialityList[3].speciality}</Item>
+                )}
+              </Grid>
+            </Grid>
+          </Box>
+        </Grid>
+        <Grid
+          container
+          sx={{
+            display: { xs: "flex" },
+            flexDirection: { xs: "column" },
+            gap: 3,
+          }}
+        >
+          <Grid
+            container
+            sx={{
+              display: { xs: "flex" },
+              flexDirection: { xs: "column" },
+              gap: 3,
+            }}
+          >
+            <Grid
+              container
+              sx={{
+                display: { xs: "flex" },
+                flexDirection: { xs: "column" },
+                gap: 1,
+              }}
+            >
+              <Typography
+                sx={{
+                  // fontWeight: "bold",
+                  color: "#16A4C3",
+                  fontSize: 20,
+                }}
+              >
+                Contact Info
+              </Typography>
+            </Grid>
+            {/* Group for nb and email */}
+            <Grid
+              container
+              sx={{
+                display: { xs: "flex" },
+                flexDirection: { xs: "column" },
+                gap: 1,
+              }}
+            >
+              <Typography
+                sx={{
+                  // fontWeight: "bold",
+                  //   color: "#16A4C3",
+                  fontSize: 20,
+                }}
+              >
+                <LocalPhoneOutlinedIcon />:{phoneNum}
+              </Typography>
+              <Typography
+                sx={{
+                  // fontWeight: "bold",
+                  //   color: "#16A4C3",
+                  fontSize: 20,
+                }}
+              >
+                <EmailOutlinedIcon />:{drEmail}
+              </Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            sx={{
+              display: { xs: "flex" },
+              flexDirection: { xs: "column" },
+              gap: 2,
+            }}
+          >
+            <Typography
+              sx={{
+                // fontWeight: "bold",
+                color: "#16A4C3",
+                fontSize: 20,
+              }}
+            >
+              Profile Bio
+            </Typography>
+            <Typography>{profileBio}</Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default PetOwnerInfoSection2;
