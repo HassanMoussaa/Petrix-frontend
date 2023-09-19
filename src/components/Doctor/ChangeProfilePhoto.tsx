c8a0aca8158b81e60ccae171643484adca9ef9b1
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-19 15:58:13 +0300

    Adjusted box design

--- src/components/Doctor/ChangeProfilePhoto.tsx
+++ src/components/Doctor/ChangeProfilePhoto.tsx
@@ -108,7 +108,19 @@ function ChangeProfilePhoto(props: ChangeProfilePhotoProps) {
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box>
+        <Box
+          sx={{
+            position: "absolute" as "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+            width: 400,
+            bgcolor: "background.paper",
+            border: "2px solid #000",
+            boxShadow: 24,
+            p: 4,
+          }}
+        >
           <Typography id="transition-modal-title" sx={{ mb: 5 }}>
             Change Profile Picture
           </Typography>
