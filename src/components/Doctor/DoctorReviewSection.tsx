f63c16bd4f0bd0a026d50922f7ee8c63641847cc
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-19 11:27:42 +0300

    Adjusted the display of reviews in the doctor page

--- src/components/Doctor/DoctorReviewSection.tsx
+++ src/components/Doctor/DoctorReviewSection.tsx
@@ -22,35 +22,37 @@ function DoctorReviewSection(props: DoctorReviewSectionProps) {
   const { reviewList } = props;
 
   return (
-    <Grid container spacing={2}>
-      {reviewList.map((review) => (
-        <Grid item key={review.id} xs={12}>
-          <Paper
-            elevation={3}
-            sx={{ p: 2, display: "flex", flexDirection: "column" }}
-          >
-            <Box display="flex" alignItems="center">
-              <img
-                src={review.petOwner.photoUrl || "/default-user-image.jpg"}
-                alt={`${review.petOwner.firstName} ${review.petOwner.lastName}`}
-                style={{
-                  width: 40,
-                  height: 40,
-                  borderRadius: "50%",
-                  marginRight: 10,
-                }}
-              />
-              <Typography variant="h6">{`${review.petOwner.firstName} ${review.petOwner.lastName}`}</Typography>
-              <Typography
-                variant="h6"
-                style={{ marginLeft: "auto" }}
-              >{`Rating: ${review.rate}`}</Typography>
-            </Box>
-            <Typography>{review.body}</Typography>
-          </Paper>
-        </Grid>
-      ))}
-    </Grid>
+    <Paper elevation={3} sx={{ p: 2, maxHeight: 500, overflow: "auto" }}>
+      <Grid container spacing={2}>
+        {reviewList.map((review) => (
+          <Grid item key={review.id} xs={12}>
+            <Paper
+              elevation={3}
+              sx={{ p: 2, display: "flex", flexDirection: "column" }}
+            >
+              <Box display="flex" alignItems="center">
+                <img
+                  src={review.petOwner.photoUrl || "/default-user-image.jpg"}
+                  alt={`${review.petOwner.firstName} ${review.petOwner.lastName}`}
+                  style={{
+                    width: 40,
+                    height: 40,
+                    borderRadius: "50%",
+                    marginRight: 10,
+                  }}
+                />
+                <Typography variant="h6">{`${review.petOwner.firstName} ${review.petOwner.lastName}`}</Typography>
+                <Typography
+                  variant="h6"
+                  style={{ marginLeft: "auto" }}
+                >{`Rating: ${review.rate}`}</Typography>
+              </Box>
+              <Typography>{review.body}</Typography>
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Paper>
   );
 }
 
