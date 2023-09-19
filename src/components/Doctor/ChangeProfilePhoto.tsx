a82ebceac6bf5f70f4e5206379ae659f3f4c1bab
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-19 15:44:56 +0300

    Added new component for imgage change

---
+++ src/components/Doctor/ChangeProfilePhoto.tsx
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  TextField,
+  Fab,
+  Modal,
+  Typography,
+  Box,
+} from "@mui/material";
+import axios from "axios";
+import getAPIBaseURL from "../../APIBaseURL";
+import EditIcon from "@mui/icons-material/Edit";
+
+interface ChangeProfilePhotoProps {
+  setNewImageUrl: React.Dispatch<React.SetStateAction<string>>;
+}
+
+function ChangeProfilePhoto(props: ChangeProfilePhotoProps) {
+  const { setNewImageUrl } = props;
+
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+  const [open, setOpen] = useState<boolean>(false);
+
+  let config = {};
+  let login_status = JSON.parse(localStorage.getItem("login") || "");
+
+  const token = login_status.token;
+  config = { headers: { Authorization: `Bearer ${token}` } };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  //   const files = event.target.files;
+  //   if (files && files.length > 0) {
+  //     setSelectedImage(files[0]);
+  //   }
+  // };
+
+  // const handleUploadImage = async () => {
+  //   if (selectedImage) {
+  //     const formData = new FormData();
+  //     formData.append("profilePicture", selectedImage);
+
+  //     try {
+  //       const response = await axios.post(
+  //         getAPIBaseURL() + "/api/changeProfilePicture", // Adjust the API endpoint
+  //         formData,
+  //         {
+  //           headers: {
+  //             "Content-Type": "multipart/form-data",
+  //           },
+  //         }
+  //       );
+
+  //       console.log("Profile picture changed:", response.data);
+  //       setOpen(false);
+  //     } catch (error) {
+  //       console.error("Error changing profile picture:", error);
+  //     }
+  //   }
+  // };
+
+  // @ts-ignore
+
+  const handleUploadImage = async (e) => {
+    e.preventDefault();
+    // @ts-ignore
+
+    const new_profile_picture = new FormData(e.currentTarget);
+    const image = new_profile_picture.get("profile_picture");
+
+    const formData = new FormData();
+    // @ts-ignore
+    formData.append("image", image);
+
+    try {
+      const response = await axios.post(
+        getAPIBaseURL() + "/users/profilePicture",
+        formData,
+        config
+      );
+      console.log("r", response);
+
+      setNewImageUrl(response.data);
+      handleClose();
+    } catch (error) {
+      console.error("Error uploading profile picture:", error);
+    }
+  };
+
+  return (
+    <div>
+      <EditIcon onClick={handleOpen} sx={{ fontSize: "15px !important" }} />
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box>
+          <Typography id="transition-modal-title" sx={{ mb: 5 }}>
+            Change Profile Picture
+          </Typography>
+
+          <Box component="form" onSubmit={handleUploadImage} sx={{ mb: 5 }}>
+            <Typography
+              style={{ color: "#000", fontWeight: 700 }}
+              sx={{ mb: 1 }}
+            >
+              Upload Image
+            </Typography>
+            <input type="file" required name="profile_picture" />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Upload Image
+            </Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  );
+}
+
+export default ChangeProfilePhoto;
