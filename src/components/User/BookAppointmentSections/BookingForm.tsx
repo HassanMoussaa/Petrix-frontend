dce810b28a14c061fd71c4bf087ac522bff51860
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-20 14:17:01 +0300

    Added fucntion to fetch available slots

--- src/components/User/BookAppointmentSections/BookingForm.tsx
+++ src/components/User/BookAppointmentSections/BookingForm.tsx
@@ -24,6 +24,7 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import moment from "moment";
 
 interface Pet {
   id: number;
@@ -44,16 +45,53 @@ interface BookingFormProps {
   handleSubmit: React.FormEventHandler<HTMLFormElement>;
   petsList: Pet[];
   clinicLocationsList: clinicLocation[];
+  docId: number;
 }
 
 function BookingForm(props: BookingFormProps) {
-  const { handleSubmit, loginError, petsList, clinicLocationsList } = props;
+  const { handleSubmit, loginError, petsList, clinicLocationsList, docId } =
+    props;
+  // correct initial value
   const [value, setValue] = React.useState<Dayjs | null>(dayjs("2022-04-17"));
   const [age, setAge] = React.useState("");
+
+  const [availableSlots, setAvailableSlots] = useState<string[]>([]);
+  const [selectedTimeSlot, setSelectedTimeSlot] = useState<string>("");
+
+  let config = {};
+  let login_status = JSON.parse(localStorage.getItem("login") || "");
+
+  const token = login_status.token;
+  config = { headers: { Authorization: `Bearer ${token}` } };
 
   const handleChange = (event: SelectChangeEvent) => {
     setAge(event.target.value);
   };
+
+  async function fetchAvailableSlots() {
+    try {
+      const response = await axios.get(
+        getAPIBaseURL() + "/petOwners/availableSlots",
+        {
+          headers: { Authorization: `Bearer ${token}` },
+          params: {
+            docId: docId,
+            // @ts-ignore
+            date: moment(value).format("MM-DD-YYYY"),
+          },
+        }
+      );
+      setAvailableSlots(response.data.availableSlots);
+      console.log("Available Slots:", response.data);
+    } catch (error) {
+      console.error("Error fetching available slots:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAvailableSlots();
+  }, [value]);
+
   return (
     <div>
       <Grid sx={{ display: "flex", flexDirection: "column" }}>
@@ -110,9 +148,33 @@ function BookingForm(props: BookingFormProps) {
             <DatePicker
               label="Date"
               value={value}
+              format="YYYY-DD-MM"
               onChange={(newValue) => setValue(newValue)}
             />
           </LocalizationProvider>
+        </FormControl>
+
+        {/* Available Time Slots */}
+        <FormControl
+          variant="standard"
+          sx={{ m: 1, maxWidth: 200, mt: 5, maxHeight: 50 }}
+        >
+          <InputLabel id="demo-simple-select-standard-label">
+            Available Time Slots
+          </InputLabel>
+          <Select
+            labelId="demo-simple-select-standard-label"
+            id="demo-simple-select-standard"
+            value={selectedTimeSlot}
+            onChange={(e) => setSelectedTimeSlot(e.target.value)}
+            label="Available Time Slots"
+          >
+            {availableSlots.map((slot, index) => (
+              <MenuItem key={index} value={slot}>
+                {slot}
+              </MenuItem>
+            ))}
+          </Select>
         </FormControl>
       </Grid>
     </div>
