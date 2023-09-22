ca4075a063822c2f5be0d8780af71e52add2c145
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-22 20:33:28 +0300

    Added component to handle maps showing doctor locations

---
+++ src/pages/GoogleMaps/DoctorsNearYou.tsx
@@ -0,0 +1,90 @@
+import React, { useEffect, useState } from "react";
+import NavBar from "../../components/NavBar/NavBar";
+import axios from "axios";
+import getAPIBaseURL from "../../APIBaseURL";
+import {
+  Box,
+  Paper,
+  Typography,
+  Snackbar,
+  Alert,
+  Button,
+  Grid,
+  Container,
+} from "@mui/material";
+import { useLocation } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import DoctorLocations from "../../components/GoogleMaps/DoctorLocations";
+interface UserType {
+  id: number;
+  type: string;
+}
+
+interface DoctorInfo {
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  country: string;
+  profile: string;
+  phone: string;
+  photoUrl: string;
+  email: string;
+  userType: UserType;
+}
+function DoctorsNearYou() {
+  const [userInfo, setUserInfo] = useState<DoctorInfo>();
+  const [doctorInfo, setDoctorInfo] = useState<DoctorInfo>();
+  const [sucessAlertOpen, setSucessAlertOpen] = useState<boolean>(false);
+  const [newImageUrl, setNewImageUrl] = useState(userInfo?.photoUrl);
+
+  let config = {};
+  let login_status = JSON.parse(localStorage.getItem("login") || "");
+
+  const token = login_status.token;
+  config = { headers: { Authorization: `Bearer ${token}` } };
+  const userType = login_status.user_type;
+  const [error, setError] = useState(Boolean);
+  const navigate = useNavigate();
+
+  async function fetchmyProfile() {
+    try {
+      let apiEndpoint;
+
+      if (userType === 1) {
+        apiEndpoint = `/petOwners/myProfile/`;
+      } else if (userType === 2) {
+        apiEndpoint = `/doctors/myProfile/`;
+      }
+
+      const response = await axios.get(getAPIBaseURL() + apiEndpoint, config);
+
+      setUserInfo(response.data);
+      setNewImageUrl(response.data.photoUrl);
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchmyProfile();
+    // fetchDoctorProfile();
+  }, []);
+
+  return (
+    <div>
+      {userInfo && (
+        <NavBar
+          imageUrl={newImageUrl}
+          setNewImageUrl={setNewImageUrl}
+          firstName={userInfo.firstName}
+          lastName={userInfo.lastName}
+          pageTitle={"AI CLASSIFICATION"}
+        />
+      )}
+      <DoctorLocations />
+    </div>
+  );
+}
+
+export default DoctorsNearYou;
