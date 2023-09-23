05a954072caf4ab8b45e95fb591ac466363cc678
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-23 10:24:38 +0300

    Added background color to the main overaly

--- src/components/NavBar/Overlay.tsx
+++ src/components/NavBar/Overlay.tsx
@@ -9,15 +9,22 @@ interface OverlayProps {
 const Overlay: React.FC<OverlayProps> = ({ children }) => {
   return (
     <div
+      className="overlayContainer"
       style={{
+        position: "relative",
         backgroundImage: `url(${
           process.env.PUBLIC_URL + "/images/signin.png"
         })`,
+        background: `linear-gradient( 
+          to right,
+          rgba(250, 105, 0, 0.5), 
+          rgba(22, 164, 195, 0.5) 
+        ), url(${process.env.PUBLIC_URL}/images/signin.png)`,
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
       }}
     >
-      <Box className="overlayContainer">{children}</Box>
+      {children}
     </div>
   );
 };
