0b8cdceda3d6d321b32d0d5d40834aa0e2b288d3
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-24 11:17:20 +0300

    Adjusted styles for review section

--- src/components/Doctor/DoctorReviewSection.tsx
+++ src/components/Doctor/DoctorReviewSection.tsx
@@ -22,41 +22,38 @@ function DoctorReviewSection(props: DoctorReviewSectionProps) {
   const { reviewList } = props;
 
   return (
-    <Paper elevation={3} sx={{ p: 2, maxHeight: 350, overflow: "auto" }}>
+    <Paper
+      elevation={3}
+      sx={{ p: 2, maxHeight: 350, overflowX: "hidden", overflowY: "auto" }}
+    >
       <Grid container spacing={2}>
         {reviewList.map((review) => (
-          <Grid item key={review.id} xs={12}>
-            <Paper
-              elevation={3}
-              sx={{ p: 2, display: "flex", flexDirection: "column" }}
-            >
-              <Box display="flex" alignItems="center">
-                <img
-                  src={
-                    review.petOwner.photoUrl ||
-                    "http://127.0.0.1:8000/images/default_profile_picture.jpg"
-                  }
-                  alt={`${review.petOwner.firstName} ${review.petOwner.lastName}`}
-                  style={{
-                    width: 40,
-                    height: 40,
-                    borderRadius: "50%",
-                    marginRight: 10,
-                  }}
+          <Grid item key={review.id} xs={12} sx={{ mb: 1 }}>
+            <Box display="flex" alignItems="center">
+              <img
+                src={
+                  review.petOwner.photoUrl ||
+                  "http://127.0.0.1:8000/images/default_profile_picture.jpg"
+                }
+                alt={`${review.petOwner.firstName} ${review.petOwner.lastName}`}
+                style={{
+                  width: 40,
+                  height: 40,
+                  borderRadius: "50%",
+                  marginRight: 10,
+                }}
+              />
+              <Typography variant="h6">{`${review.petOwner.firstName} ${review.petOwner.lastName}`}</Typography>
+              <Typography variant="h6" style={{ marginLeft: "auto" }}>
+                <Rating
+                  name="read-only"
+                  value={review.rate}
+                  precision={0.5}
+                  readOnly
                 />
-                <Typography variant="h6">{`${review.petOwner.firstName} ${review.petOwner.lastName}`}</Typography>
-                <Typography variant="h6" style={{ marginLeft: "auto" }}>
-                  {" "}
-                  <Rating
-                    name="read-only"
-                    value={review.rate}
-                    precision={0.5}
-                    readOnly
-                  />
-                </Typography>
-              </Box>
-              <Typography>{review.body}</Typography>
-            </Paper>
+              </Typography>
+            </Box>
+            <Typography>{review.body}</Typography>
           </Grid>
         ))}
       </Grid>
