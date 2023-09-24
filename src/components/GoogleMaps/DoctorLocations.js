52d469283ad03a925f5dcd88c8ccbde5a7a6324b
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-24 21:40:26 +0300

    Updated doctorLocations componant to show user-doctors locations

--- src/components/GoogleMaps/DoctorLocations.js
+++ src/components/GoogleMaps/DoctorLocations.js
@@ -1,25 +1,82 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+import axios from "axios";
+import getAPIBaseURL from "../../APIBaseURL";
 
 const DoctorLocations = () => {
   const apiKey = "AIzaSyDTfjTU0uNZK4EMSuWd5vUiMi4ShwgTlFw";
 
-  const locations = [
-    { id: 1, name: "Location 1", lat: 37.7749, lng: -122.4194 },
-    { id: 2, name: "Location 2", lat: 34.0522, lng: -118.2437 },
-    { id: 3, name: "Location 3", lat: 40.7128, lng: -74.006 },
-  ];
-
+  const [map, setMap] = React.useState(null);
+  const [userLocation, setUserLocation] = useState(null);
   const [selectedLocation, setSelectedLocation] = useState(null);
+  const [doctorsNearUser, setDoctorsNearUser] = useState([]);
   const mapRef = useRef(null);
+
+  let config = {};
+  let login_status = JSON.parse(localStorage.getItem("login") || "");
+  const token = login_status.token;
+  config = { headers: { Authorization: `Bearer ${token}` } };
+
+  const onLoad = React.useCallback(function callback(map) {
+    mapRef.current = map;
+
+    setMap(map);
+  }, []);
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setUserLocation({
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          });
+          console.log("lat", position.coords.latitude);
+          console.log("lng", position.coords.longitude);
+          getDocotrNearYou([
+            position.coords.latitude,
+            position.coords.longitude,
+          ]);
+        },
+        (error) => {
+          console.error("Error getting current location:", error);
+        }
+      );
+    } else {
+      console.error("Geolocation is not supported by this browser.");
+    }
+  }, []);
 
   const handleLocationClick = (location) => {
     setSelectedLocation(location);
-
-    if (mapRef.current) {
-      const map = mapRef.current.state.map;
+    console.log("goo", location);
+    console.log("mm", mapRef.current);
+    if (map) {
       map.panTo({ lat: location.lat, lng: location.lng });
-      map.setZoom(5);
+      map.setZoom(15);
+    }
+  };
+
+  const getDocotrNearYou = async (userLocation) => {
+    console.log("jey", userLocation);
+    if (userLocation) {
+      const lat = userLocation[0];
+      const lng = userLocation[1];
+      console.log(lat, lng);
+
+      try {
+        const response = await axios.get(
+          getAPIBaseURL() + "/users/doctors_near_you",
+          {
+            headers: { Authorization: `Bearer ${token}` },
+            params: { lat, lng },
+          }
+        );
+
+        setDoctorsNearUser(response.data);
+      } catch (error) {
+        console.error("Error fetching doctor locations:", error);
+      }
     }
   };
 
@@ -27,20 +84,34 @@ const DoctorLocations = () => {
     <LoadScript googleMapsApiKey={apiKey}>
       <div style={{ height: "100vh", width: "100vw" }}>
         <GoogleMap
-          onLoad={(map) => {
-            mapRef.current = map;
-          }}
-          center={locations[0]}
-          zoom={8}
-          mapContainerStyle={{ height: "100%", width: "100%" }}
+          onLoad={onLoad}
+          center={userLocation || { lat: 0, lng: 0 }}
+          zoom={15}
+          mapContainerStyle={{ height: "90%", width: "100vw" }}
         >
-          {locations.map((location) => (
+          {/* Display user's location if available */}
+          {userLocation && (
             <Marker
-              key={location.id}
-              position={{ lat: location.lat, lng: location.lng }}
-              onClick={() => handleLocationClick(location)}
+              position={{
+                lat: userLocation.lat,
+                lng: userLocation.lng,
+              }}
+              icon={{
+                url: "https://maps.google.com/mapfiles/ms/icons/green-dot.png",
+                scaledSize: new window.google.maps.Size(40, 40),
+              }}
             />
-          ))}
+          )}
+
+          {/* Display doctors near the user */}
+          {/* {doctorsNearUser.map((doctor) => (
+            <Marker
+              key={doctor.id}
+              position={{ lat: doctor.latitude, lng: doctor.longitude }}
+              onClick={() => handleLocationClick(doctor)}
+            />
+          ))} */}
+
           {selectedLocation && (
             <Marker
               position={{
@@ -67,27 +138,23 @@ const DoctorLocations = () => {
         >
           <h2>Locations</h2>
           <ul>
-            {locations.map((location) => (
+            {doctorsNearUser.map((doctor) => (
               <li
-                key={location.id}
-                onClick={() => handleLocationClick(location)}
+                key={doctor.id}
+                onClick={() =>
+                  handleLocationClick({
+                    lat: parseFloat(doctor.latitude),
+                    lng: parseFloat(doctor.longitude),
+                  })
+                }
                 style={{ cursor: "pointer" }}
               >
-                {location.name}
+                {doctor.doctor.firstName} {doctor.doctor.lastName}
               </li>
             ))}
           </ul>
         </div>
       </div>
-
-      {/* Selected Location Info */}
-      {/* {selectedLocation && (
-        <div>
-          <h3>Selected Location: {selectedLocation.name}</h3>
-          <p>Latitude: {selectedLocation.lat}</p>
-          <p>Longitude: {selectedLocation.lng}</p>
-        </div>
-      )} */}
     </LoadScript>
   );
 };
