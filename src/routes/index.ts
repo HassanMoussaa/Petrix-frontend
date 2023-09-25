fd085ef0cf11a37d0e8541671ff23cc985bba4dd
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-25 20:56:18 +0300

    Added Routes file

---
+++ src/routes/index.ts
@@ -0,0 +1,11 @@
+
+interface Routes {
+    name: String,
+    route: String,
+}
+
+export const ROUTES: Routes[] = [
+    {name: "HOME", route: "/"},
+    {name: "SERVICE", route: "/"},
+    {name: "AI ASSISTENT", route: "/ai-assistance"},
+]
