d1bfbc395a62c346b65b83f119d43e9f9c21a2b5
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-26 17:28:35 +0300

    Added scroll effect to cards

--- src/components/User/WelcomeSection/TopDoctors.tsx
+++ src/components/User/WelcomeSection/TopDoctors.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Grid, Box, Button, Typography, TextField, Alert } from "@mui/material";
+import { Grid, Typography, Alert } from "@mui/material";
 import getAPIBaseURL from "../../../APIBaseURL";
 import DoctorCard from "./DoctorCard";
 import axios from "axios";
@@ -35,43 +35,58 @@ function TopDoctors() {
   }, []);
 
   return (
-    <Grid container sx={{}}>
+    <Grid container sx={{ flexDirection: "column" }}>
       <Grid item xs={12}>
         <Typography
           variant="h5"
           textAlign={"center"}
-          sx={{ mb: 8 }}
+          sx={{ mb: 3 }}
           fontSize={32}
         >
           <b>Top Doctors</b>
         </Typography>
       </Grid>
-      {/* {topDoctors.map((doctor) => doctor.body)} */}
 
       {error ? (
         <Grid item xs={12}>
           <Alert severity="error">Error fetching data from the server</Alert>
         </Grid>
       ) : (
-        <Grid
-          container
-          xs={12}
-          sx={{ overflowX: "auto", display: "flex", justifyContent: "center" }}
-          ml={5}
+        <div
+          style={{
+            maxWidth: "100%",
+            overflowX: "auto",
+            scrollbarWidth: "none",
+            WebkitOverflowScrolling: "touch",
+          }}
         >
-          {topDoctors.length > 0 &&
-            topDoctors.map((doctor) => (
-              <Grid item key={doctor.id} xs={3} md={2} sx={{ p: 2 }}>
-                <DoctorCard
-                  imageUrl={doctor.photoUrl}
-                  rate={doctor.averageRate}
-                  appointmentCount={doctor.appointmentCount}
-                  title={doctor.firstName + doctor.lastName}
-                  id={doctor.id}
-                />
-              </Grid>
-            ))}
-        </Grid>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              margin: "0 auto",
+            }}
+          >
+            {topDoctors.length > 0 &&
+              topDoctors.map((doctor) => (
+                <div
+                  key={doctor.id}
+                  style={{
+                    minWidth: "200px",
+                    margin: "8px",
+                  }}
+                >
+                  <DoctorCard
+                    imageUrl={doctor.photoUrl}
+                    rate={doctor.averageRate}
+                    appointmentCount={doctor.appointmentCount}
+                    title={doctor.firstName + doctor.lastName}
+                    id={doctor.id}
+                  />
+                </div>
+              ))}
+          </div>
+        </div>
       )}
     </Grid>
   );
