975d1f4559f019aab12218294a367ae7ba96ee2f
Hassan Moussa <hassanmoussa61@gmail.com>
2023-09-27 09:43:15 +0300

    Fixed links in footer & added images

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -18,10 +18,10 @@ function Footer(props: FooterProps) {
     if (userType === "petOwner") {
       return (
         <>
-          <Link to="/ai_classification" className="footerLink">
+          <Link to="/ai_imageClassification" className="footerLink">
             AI Classification
           </Link>
-          <Link to="/ai_assistant" className="footerLink">
+          <Link to="/chatbot" className="footerLink">
             AI Assistant
           </Link>
           <Link to="/doctors_near_you" className="footerLink">
@@ -32,13 +32,13 @@ function Footer(props: FooterProps) {
     } else if (userType === "doctor") {
       return (
         <>
-          <Link to="/customized_pages" className="footerLink">
+          <Link to="/myProfile_doctor" className="footerLink">
             Customized Pages
           </Link>
-          <Link to="/ai_assistant" className="footerLink">
+          <Link to="/chatbot" className="footerLink">
             AI Assistant
           </Link>
-          <Link to="/handle_bookings" className="footerLink">
+          <Link to="/myProfile_doctor" className="footerLink">
             Handle Bookings
           </Link>
         </>
@@ -46,16 +46,16 @@ function Footer(props: FooterProps) {
     } else {
       return (
         <>
-          <Link to="/ai_classification" className="footerLink">
+          <Link to="/ai_imageClassification" className="footerLink">
             AI Classification
           </Link>
-          <Link to="/ai_assistant" className="footerLink">
+          <Link to="/chatbot" className="footerLink">
             AI Assistant
           </Link>
-          <Link to="/customized_pages" className="footerLink">
+          <Link to="/myProfile_petOwner" className="footerLink">
             Customized Pages
           </Link>
-          <Link to="/doctors_near_you" className="footerLink">
+          <Link to="/doctors-near-you" className="footerLink">
             Doctors Near You
           </Link>
         </>
@@ -78,7 +78,35 @@ function Footer(props: FooterProps) {
           justifyContent="center"
           alignItems="center"
           gap={20}
+          sx={{ position: "relative" }}
         >
+          {/* Add your image here */}
+          <Box
+            component="img"
+            src={process.env.PUBLIC_URL + "/images/Claws_footer.svg"}
+            alt="logo"
+            sx={{
+              height: "auto",
+              opacity: "0.5",
+              position: "absolute",
+              top: -30,
+              left: 0,
+            }}
+          />
+          {/* Mirror the image */}
+          <Box
+            component="img"
+            src={process.env.PUBLIC_URL + "/images/Claws_footer.svg"}
+            alt="logo"
+            sx={{
+              height: "auto",
+              opacity: "0.5",
+              position: "absolute",
+              top: -30,
+              right: 0,
+              transform: "scaleX(-1)",
+            }}
+          />
           {renderUserLinks()}
         </Grid>
         <Grid
@@ -90,7 +118,6 @@ function Footer(props: FooterProps) {
           className="footerLink"
           mb={5}
         >
-          {/* <Link to="/">PETRIX</Link> */}
           <Link to={"/"}>
             <Box
               component="img"
